5ecbf2751225251d49e4a864dea00dbb03e5cdea
Asato Wakisaka <asato.wakisaka@gmail.com>
2015-09-13 14:59:21 +0900

    ISUCON_EC2_HOST=XXX ./deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+set -e
+set -x
+me=$(whoami)
+ssh -i ~/.ssh/id_personal_ec2_rsa isucon@$ISUCON_EC2_HOST "cd ~/webapp/perl && git pull && carton install --deployment && supervisorctl restart isucon_perl"
