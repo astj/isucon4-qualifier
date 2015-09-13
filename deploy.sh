b51f777e2e0430a5e4a62463d43a1ff18c3712f6
Asato Wakisaka <asato.wakisaka@gmail.com>
2015-09-13 19:29:09 +0900

    本番ホストはアーキテクチャ違うからdeploymentやめた方がいい

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@
 set -e
 set -x
 me=$(whoami)
-ssh -i ~/.ssh/id_personal_ec2_rsa isucon@$ISUCON_EC2_HOST "cd ~/webapp/perl && git pull && carton install --deployment && supervisorctl restart isucon_perl"
+ssh -i ~/.ssh/id_personal_ec2_rsa isucon@$ISUCON_EC2_HOST "cd ~/webapp/perl && git pull && carton install && supervisorctl restart isucon_perl"
